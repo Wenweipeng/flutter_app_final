4a49621f06fccdb6544ae1685b70ecf7bc036799
Wenweipeng <1738998471@qq.com>
2022-06-13 16:38:35 +0800

    add

--- lib/routers/router.dart
+++ lib/routers/router.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import '../pages/tabs/Tabs.dart';
 
-//配置路由
 final routes = {
   '/': (context) => Tabs(),
 };
 
-//固定写法
 var onGenerateRoute = (RouteSettings settings) {
-// 统一处理
   final String? name = settings.name;
   final Function pageContentBuilder = routes[name] as Function;
   if (pageContentBuilder != null) {
